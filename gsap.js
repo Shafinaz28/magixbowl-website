141ca341aae2eca7fe4a1b6f363b744eaf56de46
Shafinaz <shafinaz348@gmail.com>
2025-09-02 11:51:39 +0530

    first commit

---
+++ gsap.js
@@ -0,0 +1,30 @@
+gsap.from(".header", {duration: 1, x: '-100%'});
+
+// gsap.from(".hh1", {duration: 1, y: '-100%',ease: "bounce"});
+
+// gsap.from(".hhimg", { 
+//     duration: 1,
+//     x: 200,
+//     rotation: 90,
+//   });
+
+// gsap.from(".hhp", {duration: 1, x: '-100%',delay:0.5});
+
+gsap.from(".nav1", {duration: 1, y: '-150%',delay: 1,ease: "bounce"});
+
+gsap.from(".nav2", {duration: 1, y: '-150%',delay: 1.1,ease: "bounce"});
+
+gsap.from(".nav3", {duration: 1, y: '-150%',delay: 1.2,ease: "bounce"});
+
+gsap.from(".nav4", {duration: 1, y: '-150%',delay: 1.3,ease: "bounce"});
+
+gsap.from(".nav5", {duration: 1, y: '-150%',delay: 1.4,ease: "bounce"});
+
+gsap.from(".hhhh", {duration: 1, opacity: 0, delay:1, stagger: 0.5});
+
+gsap.from(".hhimg", {duration: 1, y: '-150%', delay:1.5, stagger: 0.5});
+
+
+
+gsap.from(".services-card", {duration: 1, opacity: 0, delay:1.5, stagger: 0.3});
+
